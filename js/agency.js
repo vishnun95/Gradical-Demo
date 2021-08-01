ad2936e1b3122fceb6188311e1ddc14c2cb56dc6
arun-ab <arundas.gt@accubits.com>
2021-08-01 12:30:53 +0530

    career changes

--- js/agency.js
+++ js/agency.js
@@ -53,7 +53,7 @@ function initiateScroll() {
         el: document.querySelector('[data-scroll-container]'),
         smooth: true,
         smartphone: {
-            smooth: true
+            smooth: false
         },
         tablet: {
             smooth: true
--- js/projectDetails.js
+++ js/projectDetails.js
@@ -57,7 +57,7 @@ function initiateScroll() {
         el: document.querySelector('[data-scroll-container]'),
         smooth: true,
         smartphone: {
-            smooth: true
+            smooth: false
         },
         tablet: {
             smooth: true
