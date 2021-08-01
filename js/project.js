299926b890d231b12340cdf83347a9dab87fec66
arun-ab <arundas.gt@accubits.com>
2021-08-01 12:45:48 +0530

    agency, project details page

--- js/project.js
+++ js/project.js
@@ -74,7 +74,7 @@ function initiateScroll() {
         el: document.querySelector('[data-scroll-container]'),
         smooth: true,
         smartphone: {
-            smooth: true
+            smooth: false
         },
         tablet: {
             smooth: true
