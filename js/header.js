a7206a0d4c3d8a26947805c1dd89dd1d853c6bab
arun-ab <arundas.gt@accubits.com>
2021-08-01 23:14:01 +0530

    agency, project details page

--- js/header.js
+++ js/header.js
@@ -4,11 +4,9 @@ $(document).ready(function () {
         locoScroll.on('scroll', (args) => {
             // Get all current elements : args.currentElements
             if (args.direction === 'down') {
-              $('.headerOuter').addClass('moveUpHeader');
+                $('.headerOuter').addClass('moveUpHeader');
             } else {
-              if (!checkIfScrollStopped) {
                 $('.headerOuter').removeClass('moveUpHeader');
-              }
             }
           });
     }
