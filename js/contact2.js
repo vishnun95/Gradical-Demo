a1f57957ff44b86f17f494f17d9113931d030d21
Vishnu <vishnu@Vishnus-MacBook-Pro.local>
2021-08-05 19:12:45 +0530

    added contact

--- js/contact2.js
+++ js/contact2.js
@@ -1,7 +1,73 @@
 var baseUrl = 'https://d1rgwuzou45mfe.cloudfront.net';
+var emailBaseURL = 'https://d1rgwuzou45mfe.cloudfront.net'; //send mail 
+
 var locoScroll;
 var swiper;
 var types = [];
+
+function contact() {
+   
+    var contactUS = {
+        name: document.getElementById('name').value,
+        email: document.getElementById('email').value,
+        phone: document.getElementById('phone').value,
+        Description: document.getElementById('message').value, 
+        company: document.getElementById('company').value,
+        type: types.toString()
+    }
+    // let radio = document.getElementsByName('type')
+    //  for(var i = 0; i < radio.length; i++) {
+    //             if(radio[i].checked) {
+    //                 contactUS.type = radio[i].value;
+    //             }
+                
+    //         }
+   console.log(contactUS);
+   if(contactUS.name == '' && contactUS.email == '' && contactUS.phone == '' && contactUS.company == '') {
+       alert('Please fill all fields');
+       return;
+   }
+    else {
+         $.ajax({
+    url: baseUrl + "/contacts",
+    type: 'POST',
+    data: contactUS,
+    success: function  (data) {
+        console.log(data);
+        // alert('Admin team will contact you shortly');
+        document.getElementById('successText').innerHTML = 'ENQUIRY SUBMITTED SUCESSFULLY :)'
+        sendMail(contactUS)
+    },
+    error: function(error) {
+        console.log(error);
+        alert('Invalid Email or Phone Number')
+    }
+   
+  });
+    }
+}
+
+function sendMail(contact) {
+    
+    let data = {
+        email:  'hey@gradical.xyz',
+        // email: 'vishnuvn012@gmail.com',
+        message: 'Hi Team \n \n Name: '+ contact.name + '\n Email: '+ contact.email + '\n Phone: '+ contact.phone + '\n Type: ' + contact.type + '\n Message:' + contact.Description
+    }
+    //  contact.name + ' raised a query. Please reach him/her on ' + contact.phone + ' or ' + contact.email + '\n '
+    $.ajax({
+        url:'https://d2krj3pr4i8060.cloudfront.net/users',
+        type: 'POST',
+        data: data,
+        success: function (res) {
+            document.getElementById('successText').innerHTML = 'ENQUIRY SUBMITTED SUCESSFULLY :)'
+        },
+        error: function (error) {
+            console.log(error)
+        }
+
+    });
+}
 
 function customCursor() {
     var cursor = $(".cursor"),
