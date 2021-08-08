5d8b344a1404ff3e10413a950846e6f926aab3fe
Amennnnnn1 <mohammedamen84@gmail.com>
2021-08-08 12:26:23 +0530

    links linked

--- js/main.js
+++ js/main.js
@@ -478,8 +478,28 @@ function toContact() {
   window.location = 'contact.html'
 }
 
-function toNotion() {
-  window.open('https://www.notion.so/gradical/Careers-94c5975091fd4832a40fc32e0faa50ec') 
+function toCareer() {
+  window.location = 'careers.html'
+}
+
+function toFrontend() {
+  window.open('https://gradical.notion.site/Creative-Front-End-Developer-fe0cd2ec050645a1849a41ad5529475c') 
+}
+
+function toDesigner() {
+  window.open('https://gradical.notion.site/Mid-Level-Visual-Designer-be4682eeb7c34217b9855f20c8fe3239')
+}
+
+function toContent() {
+  window.open('https://gradical.notion.site/Content-Writer-28e827b7216e4917b70d299e8305e6d5')
+}
+
+function toStrategist() {
+  window.open('https://gradical.notion.site/Paid-Ads-Strategist-b71c5d8c0bbd4c3e932aa4e6a9d7612c')
+}
+
+function toMediamanager() {
+  window.open('https://gradical.notion.site/Social-Media-Manager-b0f6c373c0534b0cbd8f9290889bcc7b')
 }
 
 function toMail() {
