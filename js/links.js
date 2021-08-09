6035f6c673988cb6fe91bdb96687cb8620c5faa4
Amennnnnn1 <mohammedamen84@gmail.com>
2021-08-09 10:27:00 +0530

    link js updated

---
+++ js/links.js
@@ -0,0 +1,47 @@
+function toContact() {
+  window.location = 'contact.html'
+}
+
+function toCareer() {
+  window.location = 'careers.html'
+}
+
+function toFrontend() {
+  window.open('https://gradical.notion.site/Creative-Front-End-Developer-fe0cd2ec050645a1849a41ad5529475c') 
+}
+
+function toDesigner() {
+  window.open('https://gradical.notion.site/Mid-Level-Visual-Designer-be4682eeb7c34217b9855f20c8fe3239')
+}
+
+function toContent() {
+  window.open('https://gradical.notion.site/Content-Writer-28e827b7216e4917b70d299e8305e6d5')
+}
+
+function toStrategist() {
+  window.open('https://gradical.notion.site/Paid-Ads-Strategist-b71c5d8c0bbd4c3e932aa4e6a9d7612c')
+}
+
+function toMediamanager() {
+  window.open('https://gradical.notion.site/Social-Media-Manager-b0f6c373c0534b0cbd8f9290889bcc7b')
+}
+
+function toMail() {
+  window.location.href = "mailto:hey@gradical.xyz"
+}
+
+function toCall() {
+  window.location = "tel:+91 73561 35061"
+}
+
+function toInsta() {
+  window.open('https://www.instagram.com/gradicalstudio/')
+}
+
+function toLinkedin() {
+  window.open('https://www.linkedin.com/company/gradical') 
+}
+
+function toFacebook() {
+  window.open('https://www.facebook.com/gradicalstudio')
+}
